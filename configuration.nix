6eadbe0683f377afaf5e479f8a6545836b2aad51
Thomas Hirter <15847494+Memurame@users.noreply.github.com>
2026-08-09 21:43:07 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -28,9 +28,13 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  # KDE Plasma 6
+  services.xserver.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # SDDM display manager
+  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
